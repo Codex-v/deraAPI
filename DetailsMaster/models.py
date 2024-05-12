2fd1b8ba6802793465fd0d33a388e92daf13d8c6
ved shah <veds2103@gmail.com>
2024-05-12 14:46:01 +0000

    server data

--- DetailsMaster/models.py
+++ DetailsMaster/models.py
@@ -5,7 +5,7 @@ class DDeatils(models.Model):
     id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
     d_id = models.CharField(db_column='D_ID', unique=True, max_length=200, blank=True)  # Field name made lowercase.
     d_name = models.CharField(db_column='D_Name', max_length=200, blank=True)  # Field name made lowercase.
-    d_picurl = models.ImageField(upload_to='photos/')  # Field name made lowercase.
+    d_picurl = models.ImageField(upload_to='media/',null=True)  # Field name made lowercase.
     d_fathername = models.CharField(db_column='D_FatherName', max_length=255, blank=True)  # Field name made lowercase.
     d_address = models.TextField(db_column='D_Address', blank=True)  # Field name made lowercase.
     d_religion = models.CharField(db_column='D_Religion', max_length=150, blank=True)  # Field name made lowercase.
@@ -17,7 +17,8 @@ class DDeatils(models.Model):
     d_placevislastyear = models.CharField(db_column='D_PlaceVisLastYear', max_length=50, blank=True)  # Field name made lowercase.
     d_familydeatils = models.JSONField(db_column='D_FamilyDeatils', blank=True, null=True)  # Field name made lowercase.
     d_deradetails = models.JSONField(db_column='D_DeraDetails', blank=True, null=True)  # Field name made lowercase.
-
+    d_age = models.IntegerField(db_column='D_age', blank=True, null=True)  # Field name made lowercase.
+    
     class Meta:
         managed = False
         db_table = 'd_deatils'
